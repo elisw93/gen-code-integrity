9345fb081ad1b707919f30d11c69f6c45d9334a6
elisw93 <elwen@synspective.com>
2021-06-01 17:16:45 +0900

    comments for checkintegrity.sh

--- checkintegrity.sh
+++ checkintegrity.sh
@@ -2,16 +2,20 @@
 set -e
 set -x
 
+# scratch directory for generating files
 SCRATCH="${GITHUB_ACTION_PATH}/scratch"
 
+# parse script arguments
 GENERATE_CMD="$1"
 DIFF_ROOT="$2"
 
+# copy working-tree from checkout to scratch directory
 cp -R ${GITHUB_WORKSPACE} ${SCRATCH}
 cd ${SCRATCH}
 
+# run the generate command
 eval "${GENERATE_CMD}"
 
-# error and quit (because of -e) if there is a diff
+# error and quit if there is a diff
 diff --recursive --new-file --unified=0 {${SCRATCH},${GITHUB_WORKSPACE}}/${DIFF_ROOT} || echo "Generated files are stale. Please run \`${GENERATE_CMD}\` and check in the result." && false
 
